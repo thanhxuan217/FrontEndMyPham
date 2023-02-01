21ec1622215b74c131384cd0f296e8ddfae85de7
thanhxuan217 <101081229+thanhxuan217@users.noreply.github.com>
2023-02-01 21:41:17 +0700

    Filter by categories and categorydetail

--- cosmetic-fe/src/components/Products.vue
+++ cosmetic-fe/src/components/Products.vue
@@ -2,17 +2,24 @@
 import ProductAPI from '../api/ProductAPI/ProductAPI'
 import VueSlider from 'vue-slider-component'
 import 'vue-slider-component/theme/antd.css'
-import { reactive } from 'vue'
+import { reactive, watch, nextTick } from 'vue'
 import { onMounted } from 'vue'
 import VNDCurrencyFormatter from '../util/VNDCurrencyFormatter'
 import Card from '../components/Card.vue'
+import { RouterLink } from 'vue-router'
 const state = reactive({
     firstPrice: [0, 5000000],
     categories: [],
     discounts: [],
     products: [],
     queryParam: [],
-    filters: []
+    filters: {
+        categoryIds: [],
+        categoryDetailIds: [],
+        // discountIds: [],
+        firstPrice: null,
+        lastPrice: null
+    }
 })
 
 function toggleDropdownMenu(e) {
@@ -28,6 +35,63 @@ function toggleDropdownMenu(e) {
         e.currentTarget.className = e.currentTarget.className.split(' ')[0]
     }
 }
+async function addFilterProduct(e) {
+    const filterId = e.currentTarget.id.split(' ')
+    const id = filterId[0]
+    const category = filterId[1]
+    switch (category) {
+        case 'category':
+            // check class if active => add, else remove
+            if (e.currentTarget.className === 'category-name') {
+                e.currentTarget.className = 'category-name active'
+                state.filters.categoryIds.push(id)
+            } else {
+                e.currentTarget.className = 'category-name'
+                const newFilter = state.filters.categoryIds.filter(categoryId => Number(categoryId) !== Number(id))
+                state.filters.categoryIds = newFilter
+            }
+            break;
+        case 'categoryDetail':
+            if (e.currentTarget.className === 'category-detail') {
+                e.currentTarget.className = 'category-detail active'
+                state.filters.categoryDetailIds.push(id)
+            } else {
+                e.currentTarget.className = 'category-detail'
+                const newFilter = state.filters.categoryDetailIds.filter(categoryDetailId => Number(categoryDetailId) !== Number(id))
+                state.filters.categoryDetailIds = newFilter
+            }
+            break;
+        // case 'discount':
+        //     if (e.currentTarget.className === 'category-name') {
+        //         e.currentTarget.className = 'category-name active'
+        //         state.filters.discountIds.push(id)
+        //     } else {
+        //         e.currentTarget.className = 'category-name'
+        //         const newFilter = state.filters.discountIds.filter(categoryDetailId => Number(categoryDetailId) !== Number(id))
+        //         state.filters.discountIds = newFilter
+        //     }
+        //     break;
+        default:
+            break;
+    }
+    // access state after change
+    await nextTick()
+    ProductAPI.filterProduct(state.filters)
+        .then(res => {
+            console.log(res.data)
+            state.products = res.data
+        })
+}
+// watch(state.filters, async (newFilters, oldFilter) => {
+//     try {
+//         const res = await ProductAPI.filterProduct(newFilters)
+//         console.log(newFilters)
+//         console.log(res.data)
+//         state.products = res.data
+//     } catch (error) {
+
+//     }
+// })
 //khi query param thay doi=> load lai products
 onMounted(() => {
     ProductAPI.getAllCategory()
@@ -38,7 +102,6 @@ onMounted(() => {
 
     ProductAPI.filterProduct(state.queryParam)
         .then(res => {
-            console.log(res.data)
             state.products = res.data
         })
 })
@@ -47,7 +110,7 @@ onMounted(() => {
     <div class="product-page">
         <div class="left-page">
             <div class="breadcrumbs">
-                <Link to='/' class="none-underline">TRANG CHỦ</Link>/&nbsp;<span class="menu-title">
+                <RouterLink to='/' class="none-underline">TRANG CHỦ</RouterLink>/&nbsp;<span class="menu-title">
                 </span>
             </div>
             <div class="filter">
@@ -58,7 +121,8 @@ onMounted(() => {
                 <div class="filter-content">
                     <div class="categories" v-for="category in state.categories">
                         <div class="category">
-                            <div class="category-name">
+                            <div class="category-name" :id="category.CATEGORY_ID + ' category'"
+                                @click="addFilterProduct">
                                 {{ category.CATEGORY_NAME }}
                             </div>
                             <div class="dropdown-icon" :id="category.CATEGORY_ID + ' arrow'"
@@ -67,7 +131,8 @@ onMounted(() => {
                             </div>
                         </div>
                         <div class="category-details" :id="category.CATEGORY_ID + ' dropdown'">
-                            <div class="category-detail" v-for="categoryDetail in category.category_details">
+                            <div class="category-detail" v-for="categoryDetail in category.category_details"
+                                :id="categoryDetail.CATEGORY_DETAIL_ID + ' categoryDetail'" @click="addFilterProduct">
                                 {{ categoryDetail.CATEGORY_DETAIL_NAME }}
                             </div>
                         </div>
@@ -79,39 +144,40 @@ onMounted(() => {
                 <div class="diver"></div>
                 <div class="filter-content">
                     <div class="slider">
-                        <vue-slider v-model="state.firstPrice" drag-on-click="true" tooltip="hover" min=0 max=5000000
+                        <vue-slider v-model="state.firstPrice" tooltip="hover" min=0 max=5000000
                             :tooltip-formatter="val => VNDCurrencyFormatter.formatToVND(val)"
                             :rail-style="{ backgroundColor: 'rgb(174 171 171)' }"
                             :process-style="{ backgroundColor: 'rgb(109, 103, 103)' }"
                             :dot-style="{ border: '2px solid rgb(109, 103, 103)' }" />
                     </div>
                     <div class="btn-and-price">
-                        <button class="btn">Lọc</button>
+                        <button class="btn" @click="addFilterProduct" id="btn price">Lọc</button>
                         <div class="price">
                             Giá:
                             <span>
                                 {{
                                     VNDCurrencyFormatter.formatToVND(state.firstPrice[0]) + ': ' +
-                                    VNDCurrencyFormatter.formatToVND(state.firstPrice[1])
+                                        VNDCurrencyFormatter.formatToVND(state.firstPrice[1])
                                 }}
                                 &nbsp;
                             </span>
                         </div>
                     </div>
                 </div>
-                <div class="filter-title">
+                <!-- <div class="filter-title">
                     Khuyến mãi
                 </div>
                 <div class="diver"></div>
                 <div class="filter-content">
                     <div class="categories" v-for="discount in state.discounts">
                         <div class="category">
-                            <div class="category-name">
+                            <div class="category-name" @click="addFilterProduct"
+                                :id="discount.DISCOUNT_ID + ' discount'">
                                 {{ discount.DISCOUNT_NAME }}
                             </div>
                         </div>
                     </div>
-                </div>
+                </div> -->
             </div>
             <button class="reset-all-filter">Làm mới lọc</button>
         </div>
@@ -218,6 +284,14 @@ onMounted(() => {
     display: flex;
     justify-content: space-between;
     align-items: center;
+}
+
+.product-page .left-page .filter .filter-content .categories .category .active {
+    font-weight: bold;
+}
+
+.product-page .left-page .filter .filter-content .categories .category-details .active {
+    font-weight: bold;
 }
 
 .product-page .left-page .filter .filter-content .categories .category .active-icon {
